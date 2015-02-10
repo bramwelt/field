8f72c821d63cfd9829d9dc504ebb009c3b555341
Trevor Bramwell <trevor@bramwell.net>
2015-02-09 23:20:40 -0800

    Add real license header and set to GPLv3

--- field.py
+++ field.py
@@ -1,10 +1,20 @@
 #! /usr/bin/env python
 """
-Field
+Field - extract fields from a file
+Copyright (C) 2015 Trevor Bramwell 
 
-Written by Trevor Bramwell
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-License: GPLv2
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 import sys
 from argparse import ArgumentParser, FileType
